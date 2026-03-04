6e556d307b33666e0d09721a193d1878ad6e37cd
ikz87 <iikz87ii@gmail.com>
2026-03-04 01:14:20 -0600

    small fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -246,7 +246,7 @@ const HomePage = () => {
                 </Button>
               </div>
             )}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div className={`grid grid-cols-1 ${isRunningInDiscord()? "" : "md:grid-cols-2"} gap-2`}>
               {!isRunningInDiscord() && (
                 <Button
                   variant="ghost"
