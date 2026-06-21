e16f7c1f0acc1dbcd33a41b3699f3364ac9a884e
Nichi Hachi <nichi.hc@proton.me>
2026-06-22 01:50:16 +0200

    feat(Infernoguessr): Add graph of number of guesses in each photo

--- src/pages/play/InfernoPlayPage.tsx
+++ src/pages/play/InfernoPlayPage.tsx
@@ -10,6 +10,7 @@ import { supabase } from "../../lib/supabaseClient";
 import { CURRENT_VERSION, useVersion } from "../../context/VersionContext";
 import { Typewriter } from "../../components/Typewriter";
 import AlertDialog from "../../components/ui/AlertDialog";
+import Tooltip from "../../components/ui/Tooltip";
 import { getMsUntilNicaraguaMidnight } from "../../lib/time";
 import { useSettings } from "../../context/SettingsContext";
 
@@ -1202,9 +1203,43 @@ const InfernoPlayPage = () => {
                           />
                         </div>
                       </div>
-                        <div>
-                          <p>WIP</p>
-                      </div>
+                      {guessesFromPlayers && (
+                        <div className='flex flex-row items-end ml-4'>
+                          {levels.map((level) => {
+                            const guessCount = level.id.toString() in guessesFromPlayers ?
+                              guessesFromPlayers[level.id.toString()] :
+                              0;
+                            const percent = Math.round((guessCount / totalGuessesFromPlayers * 100));
+                            const tooltipContent = (
+                              <div className="text-center">
+                                <p>{level.levelNumber}</p>
+                                <p>Guesses: {guessCount}</p>
+                                <p>{percent}%</p>
+                              </div>
+                            );
+
+                            return (
+                              <Tooltip key={level.id} content={tooltipContent} placement="top">
+                                <div
+                                  className={`w-3 mr-0.75 opacity-50
+                                      ${lastRoundResult.correct_level.id == level.id ?
+                                      'bg-green-500' :
+                                      (lastRoundResult.guessed_level.id == level.id ?
+                                        'bg-red-500' :
+                                        'bg-white')}
+                                      `}
+                                  style={{
+                                    height: `${guessCount > 0 ?
+                                      122 * guessCount / maxGuessesFromPlayers + 12 :
+                                      12}px`,
+                                    borderRadius: '12px'
+                                  }}>
+                                </div>
+                              </Tooltip>
+                            )
+                          })}
+                        </div>
+                      )}
                     </div>
                     <motion.div
                       initial={{ opacity: 0 }}
@@ -1215,7 +1250,7 @@ const InfernoPlayPage = () => {
                           preventScroll: true,
                         });
                       }}
-                      style={{ 'display': 'flex', 'flexDirection': 'column', 'maxWidth': '160px' }}
+                      style={{ 'display': 'flex', 'flexDirection': 'column', 'maxWidth': '180px' }}
                     >
                       {isGameFinished ? (
                         <Button
